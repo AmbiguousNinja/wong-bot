603399bc3392b891cc621c384daf34f049979154
Tony Wu <tonyhuiwu@gmail.com>
2022-01-30 15:36:38 -0800

    add bot check

--- bot.py
+++ bot.py
@@ -14,11 +14,15 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_message(message):
+    # skip bot messages
+    if message.author.bot:
+        return
+
     msg_clean = message.content.lower()
     msg_raw   = message.content
 
     if "wong" in msg_clean:
-        await message.channel.send("nice")
+        await message.channel.send("wong!")
     elif msg_raw == "HASHIRIDASHITA":
         await message.channel.send("https://external-preview.redd.it/ztDm-Hvs6h1pbqYSFF3wRore5-cpi7-jM1Odpu_N9_I.jpg?auto=webp&s=4f07f33f33fd7af89c3774a737939bd5ba0ed754")
     elif "umaru" in msg_clean:
